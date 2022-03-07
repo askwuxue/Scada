717b43696225789ec0d5164acd940b93f221999a
askwuxue <972523419@qq.com>
2022-03-07 23:53:24 +0800

    feat: handler resource loading failed and report

--- src/error/index.js
+++ src/error/index.js
@@ -4,6 +4,13 @@ import { getPageURL } from '../utils/utils';
 // 忽略iframe错误
 const ignoreError = /^Script error\.?$/;
 
+// 资源加载error
+const resourceError = {
+  script: 'script',
+  link: 'link',
+  // img: ERROR_IMAGE
+};
+
 export function handleError() {
   /**
    * 捕获JavaScript错误，并立即上报
@@ -60,4 +67,52 @@ export function handleError() {
     // 防止默认处理（例如将错误输出到控制台）
     event.preventDefault();
   });
+
+  /**
+   * 处理监控资源加载异常并进行上报
+   */
+  window.addEventListener(
+    'load',
+    function () {
+      // performance.getEntriesByType('resource') 表示当前 HTML 文档中引用的所有静态资源信息，
+      // 不包括本身 HTML 信息。获取主页 html 数据，应该使用 performance.timing
+      const resources = performance.getEntriesByType('resource');
+      const rourceErrorData =
+        resources &&
+        resources.filter((value) => {
+          const { name, duration, initiatorType: type, transferSize } = value;
+
+          /**
+           * 监控资源error，响应时间超过20秒，SDK自身文件除外的所有资源error
+           * @date 2021-11-28
+           * @param {boolean} (Object.prototype.hasOwnProperty.call(resourceError, type) 监控资源
+           * @param {date}  duration 资源加载时间
+           * @param {date}  transferSize 资源大小
+           * @returns {boolean}
+           */
+          if (
+            (Object.prototype.hasOwnProperty.call(resourceError, type) &&
+              duration > 20000 &&
+              !name.includes()) ||
+            transferSize === 0
+          ) {
+            return value;
+          }
+        });
+      // 最终上报数据
+      const data = rourceErrorData.map((value) => {
+        const { initiatorType: resourceType, name: resUrl, startTime } = value;
+        return {
+          resUrl,
+          type: 'error',
+          subType: 'resource',
+          startTime,
+          resourceType,
+          pageURL: getPageURL(),
+        };
+      });
+      report(data);
+    },
+    false,
+  );
 }
